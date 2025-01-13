32962f2562a827c30a3db6e77b8e559fe4fe473c
Cristianc72004 <benalcazarc7@gmail.com>
2025-01-12 23:49:28 -0500

    Primer comentario 43

--- app/src/main/java/dev/practica/cristian_benalcazar_taller/ui/theme/Theme.kt
+++ app/src/main/java/dev/practica/cristian_benalcazar_taller/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package dev.practica.cristian_benalcazar_taller.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
